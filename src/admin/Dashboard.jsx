7003b36b1a73a3819028b2b2b80dc257b32cec5b
Roldi christh MAMPASSI MOUKIETOU <christhmoukietou@gmail.com>
2023-07-21 10:56:55 +0200

    admin panel

--- src/admin/Dashboard.jsx
+++ src/admin/Dashboard.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import style from './css/dashboard.module.css'
 
 const Dashboard = () => {
     return (
-        <div>
-            
+        <div className={style.container}>
+            <div className={style.rows}>
+                <div className={style.col}>
+                    <span>Revenu</span>
+                    <h5>700€</h5>
+                </div>
+                <div className={style.col}>
+                    <span>Revenu</span>
+                    <h5>700€</h5>
+                </div>
+                <div className={style.col}>
+                    <span>Revenu</span>
+                    <h5>700€</h5>
+                </div>
+                <div className={style.col}>
+                    <span>Revenu</span>
+                    <h5>700€</h5>
+                </div>
+            </div>
+
+            <div className={style.order} >
+                <h4>Liste des commandes</h4>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>image</th>
+                            <th>titre</th>
+                            <th>Prix</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>image</td>
+                            <td>creme</td>
+                            <td>100€</td>
+                            <td>Livré</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
